5927f71805348f551fcbc3861d7934a87534d02f
Chelsea G <chelseag@missionreadyhq.com>
2024-03-05 09:49:54 +1300

    added message and button to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,16 @@
 const Footer = () => {
   return (
     <footer className="bg-blue-500 h-1/5">
-        
-       
+      <div className="font-sans text-xl flex align-items justify-center pt-8 pb-5">
+        Get in touch with TurnersCars to find your dream car at one of our
+        locations!
+      </div>
+      <div className="font-sans text-xl flex align-items justify-center ">
+        <button className="py-2 w-[10rem] mt-4 mx-auto bg-blue-700 text-white rounded-md hover:bg-blue-800 hover:scale-110 active:scale-105 transition-all2">
+          Contact Us
+        </button>
+      </div>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
